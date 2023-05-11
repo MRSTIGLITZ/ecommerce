13e7280104b004b0df3b0cf40773b56bb04fe0e2
Adrian Garza Martinez <adrian.garza@globant.com>
2023-05-11 15:30:07 -0600

    login

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -44,3 +44,42 @@ export const register = async (req, res) => {
         console.log(err)
     }
 };
+
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        if (!email) {
+            return res.json({ error: "Email is taken"})
+        }
+        if (!password || password.length < 6) {
+            return res.json({ error: "Password must be at least  6 characters"})
+        }
+        // Check if email is taken
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.json({ error: "User not found"})
+        }
+
+        // Compare password
+        const match = await comparePassword(password, user.password);
+        if(!match) {
+            return res.json({ error: 'Wrong Password'})
+        }
+
+        // Create signed jwt
+        const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d'} );
+        // Send response
+        res.json({ 
+            user: {
+                name: user.name,
+                email: user.email,
+                role: user.role,
+                address: user.address,
+            },
+            token
+        });
+    } catch(err) {
+        console.log(err)
+    }
+};
